38efe157babe2e8ffa911cb86e21f56ce5c74731
ivan-pl <pleskaiv@gmail.com>
2022-11-21 16:57:04 +0300

    feat: add click listener

--- src/router.test.ts
+++ src/router.test.ts
@@ -21,4 +21,38 @@ describe("HistoryRouter", () => {
       expect(router.go).toBeInstanceOf(Function);
     });
   });
+
+  describe("implements api", () => {
+    let router: IRouter;
+
+    beforeEach(() => {
+      router = new HistoryRouter();
+    });
+
+    it(".go triggers by click on <a> element", () => {
+      router.go = jest.fn();
+      const button = document.createElement("button");
+      document.body.append(button);
+      const link = document.createElement("a");
+      const href = "some-link";
+      link.setAttribute("href", href);
+      document.body.append(link);
+
+      button.click();
+      expect(router.go).not.toHaveBeenCalled();
+
+      link.click();
+      expect(router.go).toHaveBeenCalled();
+      expect(router.go).toHaveBeenCalledWith(href, history.state);
+    });
+
+    it("routes to root for empty href", () => {
+      router.go = jest.fn();
+      const link = document.createElement("a");
+      document.body.append(link);
+      link.click();
+
+      expect(router.go).toHaveBeenCalledWith("/", history.state);
+    });
+  });
 });
--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,40 @@
 /* eslint-disable */
 
 type TMatch = (() => void) | string | RegExp;
+
+interface IHookArgs {
+  prevPath: string;
+  curPath: string;
+  state: any;
+}
+
 interface IParams {
-  onEnter?: () => Promise<void>;
-  onLeave?: () => Promise<void>;
-  onBeforeEnter?: () => Promise<void>;
+  onEnter?: (state: IHookArgs) => Promise<void>;
+  onLeave?: (state: IHookArgs) => Promise<void>;
+  onBeforeEnter?: (state: IHookArgs) => Promise<void>;
 }
 
 export interface IRouter {
   on(match: TMatch, params?: IParams): () => void;
-  go(url: string): void;
+  go(url: string, state: any): void;
 }
 
 export class HistoryRouter implements IRouter {
+  constructor() {
+    document.body.addEventListener("click", (event) => {
+      const el = event.target as HTMLElement;
+      if (!el.matches("a")) {
+        return;
+      }
+      event.preventDefault();
+      let url = el.getAttribute("href") ?? "/";
+      this.go(url, history.state);
+    });
+  }
+
   on(match: TMatch, params?: IParams) {
     return () => {};
   }
 
-  go(url: string): void {}
+  go(url: string, state: any): void {}
 }
